c6e4ae24473017a4f70e66258c7a9012e9f830b3
Kristoffer Kleine <kris.kleine@yahoo.de>
2010-12-11 17:31:20 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.command.install_scripts import install_scripts
+
+class post_install(install_scripts):
+
+    def run(self):
+        install_scripts.run(self)
+
+        from shutil import move
+        for i in self.get_outputs():
+            n = i.replace('.py', '')
+            move(i, n)
+            print "moving '{0}' to '{1}'".format(i, n)
+
+ID = 'org.cream.PIM'
+
+data_files = [
+    ('share/cream/{0}'.format(ID), ['src/manifest.xml'])
+]
+
+setup(
+    name = 'cream-pim-service',
+    version = '0.1',
+    author = 'The Cream Project (http://cream-project.org)',
+    url = 'http://github.com/cream/pim-service',
+    package_dir = {'pim': 'src/pim'},
+    packages = ['pim'],
+    data_files = data_files,
+    cmdclass={'install_scripts': post_install},
+    scripts = ['src/pim-service.py']
+)
